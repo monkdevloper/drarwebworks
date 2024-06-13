8a44c5d639d47e7a8c2d2874144b20e5236dfdac
monkdeveloper <abhipippalla@gmail.com>
2024-06-13 09:07:37 +0530

    worked model

--- functions/index.js
+++ functions/index.js
@@ -3,32 +3,38 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
 // MongoDB connection
 const uri =
+  process.env.MONGODB_URI ||
   "mongodb+srv://abhipippalla:d8E2PpZ6TpBYeMAo@drar.mqlgngv.mongodb.net/drar?retryWrites=true&w=majority";
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Serve the index.html file for the root path
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
-// Contact form route
+// Routes
 const contactRoute = require("./routes/contact");
 app.use("/contact", contactRoute);
 
-// Export the Express app as a Firebase Function
+const adminRoute = require("./routes/admin");
+app.use("/admin", adminRoute);
+
 exports.app = functions.https.onRequest(app);
